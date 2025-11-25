d3118161e72bf2aa335e04ebfdb03281341aa80f
Yalamarthi Girishuday <yalamarthigirishuday888@gmail.com>
2025-11-26 01:21:26 +0530

    commit

--- StoryGenerator/src/main/java/com/example/storyapp/controller/StoryController.java
+++ StoryGenerator/src/main/java/com/example/storyapp/controller/StoryController.java
@@ -1,5 +1,6 @@
-package com.example.storyapp;
+package com.example.storyapp.controller;
 
+import com.example.storyapp.service.StoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
